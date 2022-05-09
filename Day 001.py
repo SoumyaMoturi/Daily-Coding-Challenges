3bcc82a5ee79e3c6ddb3c2190ad5e44ea2c65417
SoumyaMoturi <69074611+SoumyaMoturi@users.noreply.github.com>
2022-05-09 15:55:48 +0530

    Update Day 001.py

--- Day 001.py
+++ Day 001.py
@@ -1,3 +1,15 @@
+'''
+
+This problem was recently asked by Google.
+
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+Bonus: Can you do this in one pass?
+'''
+
+
 def AddTwoNumbers(numbers,k):
     for num in numbers:
         if k-num in numbers:
@@ -5,5 +17,4 @@ def AddTwoNumbers(numbers,k):
     return False
 numbers=list(map(int,input().split()))
 k=int(input())
-l=[]
 print(AddTwoNumbers(numbers,k))

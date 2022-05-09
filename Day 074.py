a228fff9933f5cbdb24f1e6e062ea4b2ccaac836
SoumyaMoturi <69074611+SoumyaMoturi@users.noreply.github.com>
2022-05-09 18:43:35 +0530

    Update Day 074.py

--- Day 074.py
+++ Day 074.py
@@ -1,4 +1,27 @@
-def mul(n,x):
+'''
+This problem was asked by Apple.
+
+Suppose you have a multiplication table that is N by N. That is, a 2D array where the value at the i-th row and j-th column is (i + 1) * (j + 1) (if 0-indexed) or i * j (if 1-indexed).
+
+Given integers N and X, write a function that returns the number of times X appears as a value in an N by N multiplication table.
+
+For example, given N = 6 and X = 12, you should return 4, since the multiplication table looks like this:
+
+| 1 | 2 | 3 | 4 | 5 | 6 |
+
+| 2 | 4 | 6 | 8 | 10 | 12 |
+
+| 3 | 6 | 9 | 12 | 15 | 18 |
+
+| 4 | 8 | 12 | 16 | 20 | 24 |
+
+| 5 | 10 | 15 | 20 | 25 | 30 |
+
+| 6 | 12 | 18 | 24 | 30 | 36 |
+
+And there are 4 12's in the table.
+'''
+def occurance(n,x):
     c=0
     arr=[]
     r=[]
@@ -13,4 +36,4 @@ def mul(n,x):
     return c
 n=int(input())
 x=int(input())
-print(x,"is displayed ",mul(n,x)," times in the array.")
+print(x,"is displayed ",occurance(n,x)," times in the array.")
